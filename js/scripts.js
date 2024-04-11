9fad53771c47049c73cb100acbe0dd91a3c5aad1
Jae Lieder <jae.lieder@gmail.com>
2024-04-11 12:16:08 -0500

    updating showModal
    To achieve this dynamism, the showModal function will be edited to accept both the title and text as parameters, which means any time showModal is declared, a title and text argument must be given to it.

--- js/scripts.js
+++ js/scripts.js
@@ -74,6 +74,29 @@ let pokemonRepository = (function() {
   function showModal() {
     let modalContainer = document.querySelector('modal-container');
     modalContainer.classList.add(is-visible);
+
+    modalContainer.innerHTML = '';
+
+    let modal = docoment.createElement('div');
+    modal.classList.add('modal');
+
+    //add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
   }
   
   return {
